953ac8cc23ff4a10a3dc1a882b74fb69fb174563
rfaes <raphael.faes@outlook.com>
2024-11-12 17:46:35 +0100

    Removed packages and added it to chroot

--- zmystage/00-containerd/00-run.sh
+++ zmystage/00-containerd/00-run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
+
+on_chroot << EOF
 curl -LO https://dl.k8s.io/release/v1.30.0/bin/linux/arm64/kubectl
 install -m 644 kubectl "${ROOTFS_DIR}/usr/local/bin/kubectl"
 
 curl -LO https://dl.k8s.io/release/v1.31.0/bin/linux/arm64/kubeadm
 install -m 644 files/51cache "${ROOTFS_DIR}/usr/local/bin/kubeadm"
+EOF
